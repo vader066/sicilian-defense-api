820e5ec7ed810d77ccf4a5882d2ef7a5381cb434
vader066 <humphreydonkor066@gmail.com>
2025-04-30 21:41:43 +0000

    added cors package and handled cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import "module-alias/register";
 import express, { Request, Response } from "express";
+import cors from "cors";
 import env from "./env";
 import router from "./routes/router";
 
 const port = env.port;
 const app = express();
 
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use("/api", router);
 
