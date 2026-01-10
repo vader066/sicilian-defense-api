c9281a51aecd166542535285814cf44e2dd36c96
vader066 <humphreydonkor066@gmail.com>
2026-01-10 20:18:36 +0000

    feat: create repo, service, handler methods and route for createRoundRobinTournamentEndpoint

--- src/types/tournament.ts
+++ src/types/tournament.ts
@@ -10,6 +10,15 @@ export const createTournamentReqSchema = z.object({
 
 export type tournamentReq = z.infer<typeof createTournamentReqSchema>;
 
+export const createRoundRobinTournamentReqSchema = z.object({
+	playerIDs: z.array(z.string()).min(2, "At least 2 players required"),
+	tournamentName: z.string(),
+});
+
+export type createRoundRobinTournamentReq = z.infer<
+	typeof createRoundRobinTournamentReqSchema
+>;
+
 const ratingUpdateSchema = z.object({
 	playerId: z.string(),
 	newRating: z.number(),
