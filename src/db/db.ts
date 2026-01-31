b070546035281a63b47fcb9b5fd4bf2ad981c8fd
vader066 <humphreydonkor066@gmail.com>
2026-01-31 05:18:58 +0000

    fix: removed ssl bypass

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,7 +8,6 @@ export const pool = new Pool({
 	database: env.db.database,
 	port: env.db.port,
 	max: 20,
-	ssl: { rejectUnauthorized: false },
 	// idleTimeoutMillis: 30000,
 	// connectionTimeoutMillis: 2000,
 	// maxLifetimeSeconds: 60
